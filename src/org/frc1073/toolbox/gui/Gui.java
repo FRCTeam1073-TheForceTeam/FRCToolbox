9e6553c26c192c834d54506eab8c2e3ad643456d
Robert Todd Pack <todd@Roberts-iMac-Pro.local>
2020-07-12 21:52:04 -0400

    Working on build skeleton.

---
+++ src/org/frc1073/toolbox/gui/Gui.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+class Gui {
+
+
+    public static class Painter extends JPanel {
+	public void paintComponent(Graphics g) {
+	    System.out.println("*");
+	    g.setColor(Color.orange);
+	    g.fillRect(0, 0, getWidth(), getHeight());
+	    g.setColor(Color.red);
+	    g.fillOval(getWidth()/4, getHeight()/4,
+		       getWidth()/2, getHeight()/2);
+	}
+    }
+    
+    public static void main(String args[]){
+	
+       JFrame frame = new JFrame("My First GUI");
+       
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+       frame.setSize(300,300);
+       
+       JButton button = new JButton("Press");
+       Painter drawing = new Painter();
+       
+        // Adds Button to content pane of frame
+       frame.getContentPane().add(BorderLayout.NORTH, button);
+       frame.getContentPane().add(BorderLayout.CENTER, drawing);
+       frame.setVisible(true);
+    }
+}
